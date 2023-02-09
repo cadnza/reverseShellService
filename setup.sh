0591ebb99d7b23ab7a297319c27e9fd679cf7044
Jon Dayley <jonathandayley@gmail.com>
2023-02-09 07:59:19 -0800

    Add non-interactive execution

--- setup.sh
+++ setup.sh
@@ -1,5 +1,72 @@
 #!/usr/bin/env zsh
 
+# Parse arguments
+isInteractive=0
+showHelp() {
+	echo "-i          Interactive use"
+	echo "-a <string> Hostname (no port or username)"
+	echo "-p <string> Port"
+	echo "-u <string> Username"
+	echo "-k <string> Private key location (absolute path)"
+	exit 1
+}
+while getopts ":ia:p:u:k:" opt; do
+	case $opt in
+		i)
+			isInteractive=1
+			;;
+		a)
+			jaddress=$OPTARG
+			;;
+		p)
+			jport=$OPTARG
+			;;
+		u)
+			juser=$OPTARG
+			;;
+		k)
+			jkey=$OPTARG
+			;;
+		*)
+			showHelp
+			;;
+	esac
+done
+shift $((OPTIND-1))
+
+# Validate arguments
+[[ $isInteractive = 1 ]] && [[ -v jaddress ]] && showHelp
+[[ $isInteractive = 1 ]] && [[ -v jport ]] && showHelp
+[[ $isInteractive = 1 ]] && [[ -v juser ]] && showHelp
+[[ $isInteractive = 1 ]] && [[ -v jkey ]] && showHelp
+[[ $isInteractive = 0 ]] && [[ -v jaddress ]] || showHelp
+[[ $isInteractive = 0 ]] && [[ -v jport ]] || showHelp
+[[ $isInteractive = 0 ]] && [[ -v juser ]] || showHelp
+[[ $isInteractive = 0 ]] && [[ -v jkey ]] || showHelp
+
+# Get input and confirm if interactive
+[[ $isInteractive = 1 ]] && {
+	echo -e "\033[01mHostname\033[0m (no port or user)\033[01m:\033[0m"
+	read jaddress
+	echo -e "\033[01mPort:\033[0m"
+	read jport
+	echo -e "\033[01mUsername:\033[0m"
+	read juser
+	echo -e "\033[01mPrivate key location\033[0m (absolute path)\033[01m:\033[0m"
+	read jkey
+	echo -e "Settings:
+		\033[01mAddress:\033[0m  $jaddress
+		\033[01mPort:\033[0m	 $jport
+		\033[01mUser:\033[0m	 $juser
+		\033[01mKey path:\033[0m $jkey"
+	while [[ $confirmAnswer != y ]]
+	do
+		echo -e "Type \033[32my\033[0m to confirm or \033[31mn\033[0m to cancel."
+		read confirmAnswer
+		[[ $confirmAnswer = n ]] && exit 0
+	done
+}
+
 # Go to repo directory
 repoDirPre=$(dirname $0)
 cd $repoDirPre
@@ -14,29 +81,6 @@ sudo mkdir $serviceDir
 # Link main script
 echo Linking support files...
 sudo ln -s "$repoDir"/main.sh $serviceDir/main.sh
-
-# Get input
-echo -e "\033[01mAddress\033[0m (no port or user)\033[01m:\033[0m"
-read jaddress
-echo -e "\033[01mPort:\033[0m"
-read jport
-echo -e "\033[01mUser:\033[0m"
-read juser
-echo -e "\033[01mPrivate key location\033[0m (absolute path)\033[01m:\033[0m"
-read jkey
-
-# Confirm input
-echo -e "Settings:
-	\033[01mAddress:\033[0m  $jaddress
-	\033[01mPort:\033[0m     $jport
-	\033[01mUser:\033[0m     $juser
-	\033[01mKey path:\033[0m $jkey"
-while [[ $confirmAnswer != y ]]
-do
-	echo -e "Type \033[32my\033[0m to confirm or \033[31mn\033[0m to cancel."
-	read confirmAnswer
-	[[ $confirmAnswer = n ]] && exit 0
-done
 
 # Write files
 echo Writing files...
